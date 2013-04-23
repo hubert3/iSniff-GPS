207ff956fea02f8b7f02ab48afbf2140d44726bd
hubert3 <github@seiwert.com>
2013-04-24 02:30:10 +1000

    Fix URL handling so correct links are generated when app is not deployed to root on a web server (Apache WSGIScriptAlias other than /)

--- iSniff_GPS/urls.py
+++ iSniff_GPS/urls.py
@@ -6,17 +6,18 @@ from views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', ClientList.as_view()),
-    url(r'^client/(?P<slug>[:\w]+)$', ClientDetail.as_view()),
+    url(r'^$', ClientList.as_view(), name="clientlist"),
+    url(r'^client/(?P<slug>[:\w]+)$', ClientDetail.as_view(), name="client"),
     url(r'^clients/?$', ClientList.as_view()),
-    url(r'^network/(?P<ssid_or_bssid>.+)$', APDetail.as_view()),
-    url(r'^networks/?$', APList.as_view()),
-    url(r'^apple-wloc/?$', AppleWloc),
-    url(r'^apple-wloc/(?P<bssid>[:\w]+)$', AppleWloc),
-    url(r'^updateSSID$', updateSSID),
-    url(r'^locateSSID/?$', locateSSID),
-    url(r'^locateSSID/(?P<ssid>[\w\W]+)$', locateSSID),
-    url(r'^stats/?$', stats.as_view()),
+    url(r'^network/(?P<ssid_or_bssid>.+)$', APDetail.as_view(), name="network"),
+    url(r'^networks/?$', APList.as_view(), name="networks"),
+    url(r'^apple-wloc/?$', AppleWloc, name="applewloc-base"),
+    url(r'^apple-wloc/(?P<bssid>[:\w]+)$', AppleWloc, name="applewloc"),
+    url(r'^updateSSID$', updateSSID, name="updatessid"),
+    url(r'^locateSSID/?$', locateSSID, name="locatessid-base"),
+    url(r'^locateSSID/(?P<ssid>[\w\W]+)$', locateSSID, name="locatessid"),
+    url(r'^stats/?$', stats.as_view(), name="stats"),
 
     url(r'^admin/', include(admin.site.urls)),)
+
 
